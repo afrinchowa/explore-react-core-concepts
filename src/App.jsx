538dbf14c5c59b26f68f6a305a8a2159846ab342
Moriom <soya35-850@diu.edu.bd>
2023-09-08 19:12:40 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Counter from './counter'; 
+import Team from './team';
+import Users from './users';
+import Friends from './friends';
+function App() {
+  function handleClick(){
+    alert('button clicked');
+  }
+
+  const handleClick2 = () =>{
+    alert('button 2 clicked ');
+  }
+
+  const addToFive = (num) => {
+    alert(num + 5);
+  }
+  return (
+    <>    
+      <h3> React Core Concepts 2</h3> 
+      <Friends></Friends>
+      <Users></Users> 
+<Team></Team>
+      <Counter></Counter>
+       {/*button onclick="handleClick()">click me</button>  */}
+      <button onClick={handleClick}>Click me</button>   
+      <button onClick={handleClick2}>Click me 2</button>   
+      <button onClick={() =>{alert('third clicked')}}>Click me Third</button>   
+{/* vajaila */}
+      <button onClick={() => addToFive(3)}>Four</button>
+    </>
+  )
+}
+
+export default App
